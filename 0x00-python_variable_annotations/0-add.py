b28f36fa3d9060fd376cfe2aec5a6729019db8c1
Monarch <monarchofaether@gmail.com>
2024-10-12 13:49:16 -0400

    shebang line corrected, pycodestyle verified

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Program using Type Annotation to add two arguments and return their sum
 """
 
+
 def add(a: float, b: float) -> float:
-	""" Returns the sum of the two arguments supplied
-	"""
-	return a + b
+    """ Returns the sum of the two arguments supplied
+    """
+    return a + b
