d588cd9990a674d9f5df07a2bd41cf72b16091a3
Monarch <monarchofaether@gmail.com>
2024-10-12 13:57:23 -0400

    added doc, corrected shebang line

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,10 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 """ Returns the floor of a float
 """
 import math
 
 
 def floor(n: float) -> int:
+    """ Returns the floor value of the float
+    """
     return math.floor(n)
