25d2f68376ff6b8879018025b3ea874d59751e78
Monarch <monarchofaether@gmail.com>
2024-10-12 13:45:03 -0400

    function using type annotation to add two arguments

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Program using Type Annotation to add two arguments and return their sum
+"""
+
+def add(a: float, b: float) -> float:
+	""" Returns the sum of the two arguments supplied
+	"""
+	return a + b
