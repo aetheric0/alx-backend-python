d5a2e5c8a7fbae4743e94434cc6f6de5da7df438
Monarch <monarchofaether@gmail.com>
2024-10-12 14:05:00 -0400

    function that uses type annotation that converts a float supplied as argument to a string and returns the converted string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Converts the float supplied as argument to string.
+NOTE: function uses type annotation, argument (n)  must be of type float
+"""
+
+
+def to_str(n: float) -> str:
+    return str(n)
