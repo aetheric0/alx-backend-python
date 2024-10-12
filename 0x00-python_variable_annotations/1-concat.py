991b8233060b77cbc42701d4d617b5be7abe49d2
Monarch <monarchofaether@gmail.com>
2024-10-12 14:00:29 -0400

    concatenates two strings using type annotation and return the concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Concatenates two strings using type annotation.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns concatenated string
+    """
+    return str1 + str2
