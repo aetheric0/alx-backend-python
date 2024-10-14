4caf6944d0262995d5287273f21e62493eedbb83
Mornark <monarchofaether@gmail.com>
2024-10-14 12:50:21 -0400

    module wait_n replicated in this module except task_wait_random is called instead of wait_random

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Returns the delay on the in a list
+"""
+import asyncio
+task_wait_random = __import__('3-tasks').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list:
+    """ Spawns task_wait_random n times with specified max_delay and returns a list of delays.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
