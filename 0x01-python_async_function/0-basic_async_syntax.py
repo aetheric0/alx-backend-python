a68e2bb5ef7e82016e3d97d21867fe7faff9f094
Mornark <monarchofaether@gmail.com>
2024-10-14 07:33:15 -0400

    added doc for async coroutine

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,8 @@ import random
 
 
 async def wait_random(max_delay: int = 10):
+    """ Executes code after a random delay
+    """
     i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
     return i
