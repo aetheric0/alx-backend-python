aad3e1dea819a8f10fe86a0152c4a9e3415753a5
Mornark <monarchofaether@gmail.com>
2024-10-14 07:36:22 -0400

    type annotation added for return statement

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """ Executes code after a random delay
     """
     i = random.uniform(0, max_delay)
