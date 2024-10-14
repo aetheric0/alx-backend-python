f3e931ffdc460f01c84a53ce1ee5643b4a552350
Mornark <monarchofaether@gmail.com>
2024-10-14 12:53:53 -0400

    proper type annotation added

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,10 +2,11 @@
 """ Returns the delay on the in a list
 """
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Spawns task_wait_random n times with specified max_delay
     and returns a list of delays.
     """
